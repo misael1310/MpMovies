c317ebbaf009c93514ca17776430f7a8435d0b12
Misael Perez <misaelperezoficial@gmail.com>
2025-02-21 10:20:39 -0500

    feat(ui): add base UI for MpMovies test app

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,107 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useRef } from "react";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { useFetchTrendsQuery } from "./features/trending/trending-api-slice";
+import {
+  setMovies,
+  moveLeft,
+  moveRight,
+  moveUp,
+  moveDown,
+  moveNavigation,
+} from "./features/trending/trending-slice";
+
+import { NavBar } from "./layouts/MainLayout/NavBar";
+import { useCategories } from "./hooks/useCategories";
+import { useKeyboardNavigation } from "./hooks/useKeyboardNavigation";
+import { MediaGrid } from "./components/MediaGrid";
+import { ENDPOINTS } from "./constants/endpoints";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const dispatch = useAppDispatch();
+  const { data } = useFetchTrendsQuery({
+    fromPage: 1,
+    totalPages: 8,
+  });
 
-  return (
-    <>
-      <div>
-        <p className="text-9xl">Hello world!</p>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const gridColumns = 8;
+  const medias = useAppSelector((state) => state.trendingMedia.movies);
+  const { focusedRow, focusedColumn } = useAppSelector(
+    (state) => state.trendingMedia,
   );
+
+  const selectedRef = useRef<HTMLDivElement | null>(null);
+
+  const { getCategory } = useCategories();
+
+  // Update Media when loading data
+  useEffect(() => {
+    if (data) dispatch(setMovies(data.results));
+  }, [data, dispatch]);
+
+  // Ensure that the element in focus is displayed in the viewport
+  useEffect(() => {
+    if (selectedRef.current) {
+      selectedRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "center",
+      });
+    }
+  }, [focusedRow, focusedColumn]);
+
+  // Mapping of arrow keys to their respective actions
+  const arrowKeyActions: Record<string, () => void> = {
+    ArrowLeft: () => dispatch(moveLeft()),
+    ArrowRight: () => dispatch(moveRight()),
+    ArrowUp: () => dispatch(moveUp()),
+    ArrowDown: () => dispatch(moveDown()),
+  };
+
+  // Handling the Enter key
+  const handleEnter = async () => {
+    if (focusedColumn === -1) {
+      const genreId = selectedRef.current?.id || "";
+      const endpoint = genreId === "Tv" ? ENDPOINTS.tv : ENDPOINTS.movie;
+      const with_genres = genreId === "Tv" ? "" : genreId;
+      const categoryData = await getCategory({ endpoint, with_genres });
+      dispatch(setMovies(categoryData.results));
+    }
+  };
+
+  // Logic to exit menu (Escape key)
+  const handleBack = () => {
+    dispatch(moveNavigation());
+  };
+
+  // Use of the keyboard navigation hook
+  useKeyboardNavigation(
+    {
+      ...arrowKeyActions,
+      Enter: handleEnter,
+      Escape: handleBack,
+    },
+    [dispatch, focusedRow, focusedColumn, medias, getCategory],
+  );
+
+  if (data) {
+    return (
+      <>
+        <NavBar
+          selectedRef={selectedRef}
+          focusedColumn={focusedColumn}
+          focusedRow={focusedRow}
+        />
+        <MediaGrid
+          medias={medias}
+          gridColumns={gridColumns}
+          focusedRow={focusedRow}
+          focusedColumn={focusedColumn}
+          selectedRef={selectedRef}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
